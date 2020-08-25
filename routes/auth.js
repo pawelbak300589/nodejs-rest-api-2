485684e9a1a8bd0e2780b52a9799af120402ba05
Pawel Bak <pawelbak300589@gmail.com>
2020-08-25 15:45:34 +0100

    Add status functionality

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const { body } = require('express-validator');
 
 const User = require('../models/user');
 const authController = require('../controllers/auth');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
@@ -24,5 +25,11 @@ router.put('/signup', [
 ], authController.signup);
 
 router.post('/login', authController.login);
+
+router.get('/status', isAuth, authController.getUserStatus);
+
+router.patch('/status', isAuth, [
+    body('status').trim().not().isEmpty()
+], authController.updateUserStatus);
 
 module.exports = router;
