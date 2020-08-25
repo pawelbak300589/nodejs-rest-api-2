4fbfba037acf058de5fd8650571bcaba21ea8135
Pawel Bak <pawelbak300589@gmail.com>
2020-08-25 14:00:38 +0100

    Add user signup route (with validation) and controller method

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const uuid = require('uuid');
 const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -38,12 +39,14 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
-    res.status(status).json({ message: message });
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
 });
 
 mongoose.connect(process.env.MONGODB_CLIENT_URL, { useNewUrlParser: true, useUnifiedTopology: true })
