54bcdb1b4d1bcc101f2eca71710ca7da0577ecb0
Pawel Bak <pawelbak300589@gmail.com>
2020-08-25 11:12:00 +0100

    Update creating new post to save uploaded post image file

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,20 +3,19 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [
-            {
-                _id: '1',
-                title: 'First Post',
-                content: 'This is the first post!',
-                imageUrl: 'images/books.jpg',
-                creator: {
-                    name: 'Pawel'
-                },
-                createdAt: new Date()
+    Post.find()
+        .then(posts => {
+            res.status(200).json({
+                message: 'Fetched posts successfully.',
+                posts: posts
+            });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
             }
-        ]
-    });
+            next(err);
+        });
 };
 
 exports.createPost = (req, res, next) => {
@@ -26,12 +25,18 @@ exports.createPost = (req, res, next) => {
         error.statusCode = 422;
         throw error;
     }
+    if (!req.file) {
+        const error = new Error('No image provided.');
+        error.statusCode = 422;
+        throw error;
+    }
+    const imageUrl = req.file.path.replace("\\" ,"/");
     const title = req.body.title;
     const content = req.body.content;
     const post = new Post({
         title,
         content,
-        imageUrl: 'images/books.jpg',
+        imageUrl,
         creator: {
             name: 'Pawel'
         },
@@ -51,4 +56,23 @@ exports.createPost = (req, res, next) => {
             next(err);
         });
 
+};
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Could not find post.');
+                error.statusCode = 404;
+                throw error;
+            }
+            res.status(200).json({ message: 'Post fetched', post: post });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 };
