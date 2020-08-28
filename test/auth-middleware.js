6e4fc36a64c67bdd3511bdc4ad1dadb41e51a892
Pawel Bak <pawelbak300589@gmail.com>
2020-08-28 11:43:55 +0100

    Add description to auth middleware for better organization

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -2,12 +2,27 @@ const expect = require('chai').expect;
 
 const authMiddleware = require('../middleware/is-auth');
 
-it('should throw an error if no authorisation header is present', function () {
-    const req = {
-        get: function () {
-            return null;
-        }
-    };
+describe('Auth middleware', function () {
+    it('should throw an error if no authorisation header is present', function () {
+        const req = {
+            get: function () {
+                return null;
+            }
+        };
 
-    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not authenticated.');
+        expect(authMiddleware.bind(this, req, {}, () => {
+        })).to.throw('Not authenticated.');
+    });
+
+    it('should throw an error if authorisation header is only single string', function () {
+        const req = {
+            get: function () {
+                return 'xyz';
+            }
+        };
+
+        expect(authMiddleware.bind(this, req, {}, () => {
+        })).to.throw();
+    });
 });
+
